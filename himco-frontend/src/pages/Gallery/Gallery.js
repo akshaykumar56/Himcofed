fae427f7337cf724dab9f63c6c52598091f46d54
unknown <kshykumr@gmail.com>
2023-11-15 15:41:18 +0530

    v12.00

--- himco-frontend/src/pages/Gallery/Gallery.js
+++ himco-frontend/src/pages/Gallery/Gallery.js
@@ -9,7 +9,7 @@ function Gallery() {
         Gallery
         </div>
         <div className='gallery-text' >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque gravida sed tellus non ultrices. Aliquam feugiat et tortor at eleifend. Duis vitae ligula sed massa rhoncus dignissim. Cras id quam laoreet lectus luctus efficitur in et mi. Suspendisse sit amet dapibus arcu, vel porttitor nisl. Ut malesuada ullamcorper luctus. Duis orci nibh, interdum varius mollis non, tempus tristique enim.
+        {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque gravida sed tellus non ultrices. Aliquam feugiat et tortor at eleifend. Duis vitae ligula sed massa rhoncus dignissim. Cras id quam laoreet lectus luctus efficitur in et mi. Suspendisse sit amet dapibus arcu, vel porttitor nisl. Ut malesuada ullamcorper luctus. Duis orci nibh, interdum varius mollis non, tempus tristique enim. */}
         </div>
       </div>
       <div className='carousel-gallery'>
--- himco-frontend/src/pages/Notification/Notification.js
+++ himco-frontend/src/pages/Notification/Notification.js
@@ -12,7 +12,7 @@ function Notification(props) {
         <thead style={{backgroundColor:'#B80000'}}>
           <tr style={{color:'white'}}>
             <th style={{padding:'0px 10px'}}>Heading</th>
-            <th style={{padding:'0px 10px'}}>Description</th>
+            <th className='Notification-Description' style={{padding:'0px 10px'}}>Description</th>
             <th style={{padding:'0px 10px'}}>Time</th>
             <th style={{padding:'0px 10px'}}>Download</th>
             </tr>
@@ -20,7 +20,7 @@ function Notification(props) {
         <tbody >
     {arr.map((element)=>{ return <tr key={element.id} style={{borderBottom:'1px solid black'}}>
         {element.attributes.Heading?<td style={{padding:'0px 10px'}}>{element.attributes.Heading}</td>:<td style={{padding:'0px 10px'}}>...</td>}
-        {element.attributes.Description?<td style={{padding:'0px 10px'}}>{element.attributes.Description}</td>:<td style={{padding:'0px 10px'}}>...</td>}
+        {element.attributes.Description?<td className='Notification-Description' style={{padding:'0px 10px'}}>{element.attributes.Description}</td>:<td className='Notification-Description' style={{padding:'0px 10px'}}>...</td>}
         <td style={{padding:'0px 10px'}}>{formatMyDate(element.attributes.createdAt)}</td>
         {element.attributes.Download.data?<td style={{padding:'0px 10px'}}><a href={`${process.env.REACT_APP_SERVERNAME}${element.attributes.Download.data[0].attributes.url}`}>Download</a></td>:<td style={{padding:'0px 10px'}}>...</td>}
         </tr>
